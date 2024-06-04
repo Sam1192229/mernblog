40b1817c3b9199af85a0e190aa4fec013ffbe5c8
Sam1192229 <121726836+Sam1192229@users.noreply.github.com>
2024-06-04 17:19:13 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -21,7 +21,7 @@ const PORT = process.env.PORT || 4000;
 
 app.use(cors({
   origin: 'https://mernblog-client.vercel.app',
-  methods: ["POST","GET"],
+  methods: ["POST","GET","PUT"],
   credentials: true,
 }));
 
