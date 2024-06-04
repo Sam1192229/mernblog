91b8e1ae28bd5e42e6bd83caa0e829b25332bafe
Sam1192229 <121726836+Sam1192229@users.noreply.github.com>
2024-06-04 15:35:18 +0530

    Update EditPost.jsx

--- client/src/pages/EditPost.jsx
+++ client/src/pages/EditPost.jsx
@@ -12,7 +12,7 @@ export default function EditPost() {
   const [redirect2, setRedirect2] = useState(false);
 
   useEffect(() => {
-    fetch('http://localhost:4000/post/' + id)
+    fetch('https://mernblog-api-one.vercel.app/post/' + id)
       .then(response => {
         response.json().then(postInfo => {
           setTitle(postInfo.title);
