f8465a37acc7135eac10f4564c36dc7a526b4cd5
Sam1192229 <121726836+Sam1192229@users.noreply.github.com>
2024-09-10 09:53:59 +0530

    Update IndexPage.jsx

--- client/src/pages/IndexPage.jsx
+++ client/src/pages/IndexPage.jsx
@@ -1,10 +1,11 @@
 import Post from "../components/Post";
 import {useEffect, useState} from "react";
+import indexpic from '../assets/indexp2.avif';
 
 export default function IndexPage() {
   const [posts,setPosts] = useState([]);
   useEffect(() => {
-    fetch('https://mernblog-api-one.vercel.app/post')
+    fetch('http://localhost:4000/post')
     .then(response => {
       response.json()
       .then(posts => {
@@ -14,7 +15,13 @@ export default function IndexPage() {
     });
   }, []);
   return (
-    <div className="flex flex-col pt-20 gap-10">
+    <div className="flex flex-col pt-20 gap-10"
+    style={{
+      backgroundImage: `url(${indexpic})`, // Replace with your image path
+      backgroundSize: 'cover', // Cover the entire form area
+      backgroundPosition: 'center', // Center the image
+    }}
+    >
       {posts.length > 0 && posts.map(post => (
        
         <Post {...post}  key={post.id} />
