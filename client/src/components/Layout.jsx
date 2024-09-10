86a265f668e103141ba9420bcfca6cc2fba4bb6b
Sam1192229 <121726836+Sam1192229@users.noreply.github.com>
2024-09-10 09:52:55 +0530

    Update Layout.jsx

--- client/src/components/Layout.jsx
+++ client/src/components/Layout.jsx
@@ -1,13 +1,21 @@
 import Footer from "./Footer";
 import Header from "./Header";
-import {Outlet} from "react-router-dom";
+import { Outlet } from "react-router-dom";
+import blogImage from '../assets/temp.jfif';
 
 export default function Layout() {
   return (
-    <main className="bg-gray-800 min-h-screen">
+    <div className="flex flex-col min-h-screen bg-gray-800 ">
+      style={{
+      backgroundImage: `url(${blogImage})`, // Replace with your image path
+      backgroundSize: 'cover', // Cover the entire form area
+      backgroundPosition: 'center', // Center the image
+    }}
       <Header />
-      <Outlet />
-      <Footer/>
-    </main>
+      <main className="flex-grow">
+        <Outlet />
+      </main>
+      <Footer />
+    </div>
   );
 }
