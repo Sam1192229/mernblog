b529bc94cf94213e562bb4fa88868c5b10840f9d
Sam1192229 <121726836+Sam1192229@users.noreply.github.com>
2024-09-10 09:54:42 +0530

    Update PostPage.jsx

--- client/src/pages/PostPage.jsx
+++ client/src/pages/PostPage.jsx
@@ -3,6 +3,7 @@ import { useParams } from "react-router-dom";
 import { formatISO9075 } from "date-fns";
 import { UserContext } from "../UserContext";
 import { Link } from 'react-router-dom';
+import indexpic from '../assets/indexp2.avif';
 
 export default function PostPage() {
   const [postInfo, setPostInfo] = useState(null);
@@ -10,7 +11,7 @@ export default function PostPage() {
   const { id } = useParams();
 
   useEffect(() => {
-    fetch(`https://mernblog-api-one.vercel.app/post/${id}`)
+    fetch(`http://localhost:4000/post/${id}`)
       .then(response => {
         response.json().then(postInfo => {
           setPostInfo(postInfo);
@@ -21,13 +22,17 @@ export default function PostPage() {
   if (!postInfo) return '';
 
   return (
-    <div className="post-page bg-gray-800 text-gray-200 flex flex-col gap-4 justify-center items-center mt-10">
-      <h1 className="text-3xl font-bold text-gray-200">{postInfo.title}</h1>
-      <time className="text-gray-500">{formatISO9075(new Date(postInfo.createdAt))}</time>
-      <div className="author text-gray-500">by @{postInfo.author.username}</div>
+    <div className="post-page bg-gray-800 text-gray-200 flex flex-col gap-4 justify-center items-center pt-10 font-serif" style={{
+      backgroundImage: `url(${indexpic})`, // Replace with your image path
+      backgroundSize: 'cover', // Cover the entire form area
+      backgroundPosition: 'center', // Center the image
+    }}>
+      <h1 className="text-4xl font-extrabold text-teal-800">{postInfo.title}</h1>
+      <time className="text-gray-800">{formatISO9075(new Date(postInfo.createdAt))}</time>
+      <div className="author text-gray-800">by @{postInfo.author.username}</div>
       {userInfo.id === postInfo.author._id && (
         <div className="edit-row">
-          <Link className="edit-btn text-teal-500 hover:text-teal-700 flex items-center" to={`/edit/${postInfo._id}`}>
+          <Link className="edit-btn text-teal-900 text-2xl hover:text-teal-700 flex items-center" to={`/edit/${postInfo._id}`}>
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 mr-1">
               <path strokeLinecap="round" strokeLinejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
             </svg>
@@ -36,9 +41,9 @@ export default function PostPage() {
         </div>
       )}
       <div className="image">
-        <img src={`https://mernblog-api-one.vercel.app/${postInfo.cover}`} alt="" className="w-96 h-auto rounded-lg shadow-md mb-4" />
+        <img src={`http://localhost:4000/${postInfo.cover}`} alt="" className=" w-auto h-72 rounded-lg shadow-md mb-4" />
       </div>
-      <div className="content ml-20 mr-20 mb-20 text-wrap" dangerouslySetInnerHTML={{ __html: postInfo.content }} />
+      <div className="content ml-20 mr-20 mb-20 text-wrap text-gray-800 font-bold" dangerouslySetInnerHTML={{ __html: postInfo.content }} />
     </div>
   );
 }
