d7e8f2095c618328b1b738ddf1ec76d32e459bec
Sam1192229 <121726836+Sam1192229@users.noreply.github.com>
2024-09-10 09:52:26 +0530

    Update Footer.jsx

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,17 +1,15 @@
 import React from 'react';
-import logo from '../../img/logo.png';
 
 const Footer = () => {
   return (
-    <div className='flex flex-col md:flex-row  justify-evenly items-center gap-8 text-white bg-gray-900 w-full md:h-16 '>
-      <div className=''>
-        <img src={logo} className="h-8" alt="Samriddhi Logo" />
+    <div className='flex flex-col md:flex-row justify-between items-center gap-8 text-white bg-gray-900 w-full md:h-10 p-4'>
+      <div className='flex-1 flex justify-center'>
+        <span className="block text-sm font-bold text-teal-500 hover:underline ml-52">
+          © 2024, by Samriddhi Mishra
+        </span>
       </div>
-      <div className='md:text-center'>
-        <span className="block text-sm text-gray-500 dark:text-gray-400 hover:underline">© 2024, Built with love by Samriddhi Mishra</span>
-      </div>
-      <div className=''>
-        <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
+      <div className='flex justify-end'>
+        <ul className="flex flex-wrap items-center mb-0 text-sm font-medium text-gray-500 dark:text-gray-400">
           <li>
             <a href="#" className="hover:underline me-4 md:me-6">Linkedin</a>
           </li>
