a47fbc61fc6cfc520b7e0a797bda979b7d16e6fe
Sam1192229 <121726836+Sam1192229@users.noreply.github.com>
2024-09-10 09:52:41 +0530

    Update Header.jsx

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -6,7 +6,7 @@ export default function Header() {
   //context to set a global value for the header
   const { setUserInfo, userInfo } = useContext(UserContext);
   useEffect(() => {
-    fetch("https://mernblog-api-one.vercel.app/profile", {
+    fetch("http://localhost:4000/profile", {
       credentials: "include",
     }).then((response) => {
       response.json().then((userInfo) => {
@@ -16,7 +16,7 @@ export default function Header() {
   }, []);
 
   function logout() {
-    fetch("https://mernblog-api-one.vercel.app/logout", {
+    fetch("http://localhost:4000/logout", {
       credentials: "include",
       method: "POST",
     });
@@ -27,13 +27,13 @@ export default function Header() {
 
   return (
     <header>
-      <nav className="flex flex-row gap-4 pl-4 pr-16 justify-between p-4 bg-gray-900 text-white">
+      <nav className="flex flex-row gap-4 pl-4 pr-16 justify-between p-2 bg-gray-900 text-white">
         <div>
-          <NavLink to="/" className="logo text-teal-500 animate-fall2 ml-10 font-mono text-xl p-0 font-bold">
+          <NavLink to="/" className="logo text-teal-500 animate-fall2 ml-10 font-mono text-2xl p-0 font-bold">
           BlogBeam
           </NavLink>
         </div>
-        <div className="flex flex-row gap-16 ">
+        <div className=" flex flex-row gap-16 font-bold  font-mono text-gray-400 text-xl hover:text-white ">
           {username && (
             <>
               <NavLink to="/create">Create New Post</NavLink>
